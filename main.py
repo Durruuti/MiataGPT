c997a1e738d906cb255254216dee31fed466970e
Durruti <88114770+Durruuti@users.noreply.github.com>
2023-04-16 02:24:17 +0200

    Updates visuales

--- main.py
+++ main.py
@@ -2,56 +2,66 @@ import openai
 import config
 import typer
 from rich import print
+from rich.console import Console
 from rich.table import Table
+
+console = Console()
+
+# Constantes
+EXIT_COMMAND = "exit"
+NEW_CONVERSATION_COMMAND = "new"
+MAX_MESSAGES = 10
 
 def main():
 
     openai.api_key = config.api_key
 
-    print("[bold red]MIATAGPT (CHATGPTAPI) en Python[/bold red]")
+    console.print("[bold red]MIATAGPT (CHATGPTAPI) en Python[/bold red]")
 
     table = Table("Comando", "Descripción")
-    table.add_row("exit", "Salir de la App")
-    table.add_row("new", "Crear una nueva conversación")
+    table.add_row(EXIT_COMMAND, "Salir de la App")
+    table.add_row(NEW_CONVERSATION_COMMAND, "Crear una nueva conversación")
 
-    print(table)
+    console.print(table)
 
     # Contexto del asistente
-
     context = {"role": "system", "content": "Eres un asistente de automoción japonesa"} 
-    mensajes = [context]
+    messages = [context]
+
     while True:
+        content = __prompt()
 
-        contenido = __prompt()
+        if content == NEW_CONVERSATION_COMMAND:
+            messages = [context]
+            content = __prompt()
         
-        if contenido == "new":
-            mensajes = [context]
-            contenido = __prompt()
-        
-        mensajes.append({"role": "user", "content": contenido})
+        if content == EXIT_COMMAND:
+            if typer.confirm("¿Estás seguro que deseas salir?"):
+                console.print("¡Hasta luego!")
+                raise typer.Abort()
 
-        respuesta = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=mensajes)
-        
-        respuesta_content = respuesta.choices[0].messafe.content
-        
-        mensajes.append({"role": "assistant", "content": respuesta_content})
+        messages.append({"role": "user", "content": content})
 
-        print(f"[green]{respuesta_content}[/green]")
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"\n\n\nUsuario: {messages[-1]['content']}\nAsistente: ",
+            max_tokens=50,
+            temperature=0.5,
+            n=1,
+            stop=None,
+            messages=messages[-MAX_MESSAGES:],
+        )
+
+        response_text = response.choices[0].text.strip()
+        messages.append({"role": "assistant", "content": response_text})
+
+        console.print(f"[bold blue]Asistente:[/bold blue] {response_text}")
 
 
 def __prompt() -> str:
     prompt = typer.prompt("\n¿Sobre qué quieres hablar? ")
         
-    if prompt == "exit":
-            exit = typer.confirm("¿Estás seguro? ")
-            if exit:
-                print("Hasta luego! ")
-                raise typer.Abort()
-            
-            return __prompt()
-
-    return prompt
+    return prompt.lower().strip()
 
 if __name__ == "__main__":
     typer.run(main)
